338edc757195dd5e590b0c85cfdeea15e1339438
AuroraRyan0301 <auroraryan0301@gmail.com>
2025-04-17 16:40:57 +0000

    modify init script

--- sh_scripts/test.sh
+++ sh_scripts/test.sh
@@ -1,7 +1,7 @@
 # 环境变量配置
 MODEL_FLAGS="--image_size 256 --num_channels 256 --num_res_blocks 3 --num_heads 4 --num_head_channels 64 --num_heads_upsample -1 --learn_sigma True"
 DIFFUSION_FLAGS="--diffusion_steps 1000 --noise_schedule linear --rescale_learned_sigmas False --rescale_timesteps False --use_scale_shift_norm False"
-TRAIN_FLAGS="--lr_anneal_steps 120000 --lr 4e-5 --batch_size 12"
+TRAIN_FLAGS="--lr_anneal_steps 120000 --lr 4e-5 --batch_size 4"
 
 # 运行训练脚本，并将其放入后台
 python3 /root/HDR_diffusion/scripts/image_train.py --data_dir /data2/infinigen_processed_v0 $MODEL_FLAGS $DIFFUSION_FLAGS $TRAIN_FLAGS
